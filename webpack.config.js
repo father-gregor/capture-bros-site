88695f68a972d670097734bb66e5eb58eaf88436
Denys Rakov <bobsixpackbob@gmail.com>
2021-02-02 17:42:34 +0200

    webpack: Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const _ = require('lodash');
-const fs = require('fs');
-const path = require('path');
+const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -11,105 +10,112 @@ const clientConfig = require('./config/client.json');
 
 _.templateSettings.imports = {
     config: clientConfig,
-}
+};
 
-module.exports = {
-    entry: [
-        './src/client/ts/index.ts',
-        './src/client/scss/style.scss'
-    ],
-    output: {
-        filename: './js/bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.html$/,
-                loader: "underscore-template-loader",
-                query: {
-                    engine: 'lodash',
-                    withImports: true
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
+module.exports = (env, options) => {
+    return {
+        entry: [
+            './src/client/ts/index.ts',
+            './src/client/scss/style.scss'
+        ],
+        output: {
+            filename: './js/bundle.js'
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.html$/,
+                    loader: 'underscore-template-loader',
+                    query: {
+                        engine: 'lodash',
+                        withImports: true
+                    }
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                                postcssOptions: {
+                                    plugins: [
+                                        require('postcss-import'),
+                                        require('tailwindcss')
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
                         }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            postcssOptions: {
-                                plugins: [
-                                    require('postcss-import'),
-                                    require('tailwindcss')
-                                ]
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|jpeg|svg|gif|ico)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'public/img/'
                             }
                         }
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg|jpeg|svg|gif|ico)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'public/img/'
+                    ]
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'public/fonts/'
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'public/fonts/'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: '!!underscore-template-loader!./src/client/html/views/index.html',
-            templateParameters: {
-                config: clientConfig
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {from: './src/client/favicon', to: './public/favicon'},
-                {from: './src/client/img', to: './public/img'}
+                    ]
+                }
             ]
-        }),
-        new CleanWebpackPlugin({})
-    ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                ENV: JSON.stringify(options.mode)
+            }),
+            new HtmlWebpackPlugin({
+                template: '!!underscore-template-loader!./src/client/html/views/index.html',
+                templateParameters: {
+                    config: clientConfig
+                },
+                cache: false
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {from: './src/client/favicon', to: './public/favicon'},
+                    {from: './src/client/img', to: './public/img'},
+                    {from: './src/client/fonts', to: './public/fonts'}
+                ]
+            }),
+            new CleanWebpackPlugin({})
+        ]
+    };
 };
