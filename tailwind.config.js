2b341d0b1592b9f9bee2184c50c91a9433895822
Denys Rakov <bobsixpackbob@gmail.com>
2021-02-02 17:42:14 +0200

    config: Configure tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        primary: ['"Gnuolane Rg"', ...defaultTheme.fontFamily.sans],
-      }
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'capture-bros': ['Mastodon'],
+                logo: ['"Gnuolane Rg"', ...defaultTheme.fontFamily.sans],
+                heading: ['"Montserrat Black"'],
+                text: ['Lato']
+            },
+            colors: {
+                capture: {
+                    red: '#d20d12'
+                }
+            }
+        },
+        backgroundColor: (theme) => ({
+            ...theme('colors'),
+            'background': '#DEE2E5'
+        })
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
 }
