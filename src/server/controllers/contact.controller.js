4f307bfe9be101479454faaecf3010e06809b602
Denys Rakov <bobsixpackbob@gmail.com>
2021-02-02 20:10:46 +0200

    feat: Implement ability to save contact form that were failed to be send

--- src/server/controllers/contact.controller.js
+++ src/server/controllers/contact.controller.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const ProtonMail = require('protonmail-api');
 const {body, validationResult} = require('express-validator');
 
@@ -10,10 +11,6 @@ const protonCredentials = {
 };
 
 async function testEmailSend (req, res) {
-    if (process.env.NODE_ENV === 'production' && req.query.testing !== secureConfig.secretParam) {
-        return res.status(400).send('Testing denied');
-    }
-
     try {
         const proton = await ProtonMail.connect(protonCredentials);
 
@@ -40,15 +37,37 @@ function validateContactForm () {
     ];
 }
 
-async function sendContactForm (req, res) {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                status: 'error',
-                reason: errors.array()[0].msg
-            });
+async function getMissedEmails (req, res) {
+    const dir = `${__dirname}/../missed-emails/`;
+    const missedEmailFiles = fs.readdirSync(dir);
+    const missedEmails = [];
+    const errors = [];
+    for (let filename of missedEmailFiles) {
+        try {
+            if (!filename.startsWith('missed-email')) {
+                continue;
+            }
+            const missedEmailJSON = fs.readFileSync(dir + filename, {encoding: 'utf-8'});
+            missedEmails.push({...JSON.parse(missedEmailJSON), filename});
         }
+        catch (err) {
+            errors.push(`Failed to open file ${filename}`);
+            console.log(err);
+        }
+    }
+    return res.json({missedEmails, errors});
+}
+
+async function sendContactForm (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            status: 'error',
+            reason: errors.array()[0].msg
+        });
+    }
+
+    try {
         /*
         const proton = await ProtonMail.connect(protonCredentials);
     
@@ -64,12 +83,22 @@ async function sendContactForm (req, res) {
     }
     catch (err) {
         console.error(err);
-        return res.status(500).json({status: 'error', reason: `Internal error. Please try again later or contact directly via our email ${clientConfig.general.contactEmail}`});
+        fs.writeFile(`${__dirname}/../missed-emails/missed-email-${req.body.email}-${Date.now()}.json`, JSON.stringify(req.body, null, 4), {}, (err) => {
+            if (!err) {
+                return;
+            }
+
+            console.log(err);
+            console.log('\nFailed to save missed email, fallback to logging directly to console:');
+            console.dir(req.body, {depth: 5});
+        });
+        return res.status(400).json({status: 'error', reason: `Internal error. Please try again later or contact directly via ${clientConfig.general.contactEmail}`});
     }
 }
 
 module.exports = {
     testEmailSend,
+    getMissedEmails,
     validateContactForm,
     sendContactForm
 };
