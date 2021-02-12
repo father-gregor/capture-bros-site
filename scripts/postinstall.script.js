79dbba65b4dff254f73ba557e9cdc6520aee989f
Denys Rakov <bobsixpackbob@gmail.com>
2021-02-12 20:03:05 +0200

    refactor: Update postinstall logs

--- scripts/postinstall.script.js
+++ scripts/postinstall.script.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const puppeteerPath = path.resolve('node_modules/protonmail-api/node_modules/puppeteer');
-console.log(puppeteerPath);
+console.log('Postinstall script: folder', puppeteerPath);
 const isExists = fs.existsSync(puppeteerPath);
-console.log(`Postinstall script: folder ${puppeteerPath} exists - `, isExists);
+console.log('Postinstall script: before remove folder exists - ', isExists);
 fs.rmdirSync(path.resolve('node_modules/protonmail-api/node_modules/puppeteer'), {recursive: true});
-console.log('Postinstall script: finished, folder exists - ', fs.existsSync(puppeteerPath));
+console.log('Postinstall script: after remove folder exists - ', fs.existsSync(puppeteerPath));
